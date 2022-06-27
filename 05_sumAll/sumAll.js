80a1a71914cd4f6e3bcb9ded3de5ca1ea530cf16
Andrew Jia <andrew.jia.2013@gmail.com>
2022-06-27 16:42:12 -0700

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(small, large) {
+    //check types
+    if(typeof(small) != 'number' 
+        || typeof(large) != 'number') return 'ERROR';
+    //check order
+    if(small > large) {
+        let temp = large;
+        large = small;
+        small = temp;
+    }
+    //check negatives
+    if(small < 0) return 'ERROR';
 
+    let sum = 0;
+    for(let n = small; n <= large; n++) {
+        sum += n;
+    }
+    return sum;
 };
 
 // Do not edit below this line
