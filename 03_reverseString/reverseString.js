1e90c92c9bd6bed2defea426c6853bfb85ab892c
Andrew Jia <andrew.jia.2013@gmail.com>
2022-06-27 16:24:35 -0700

    complete 03
    should've used .join()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split('');
+    let out = '';
+    for(let i = arr.length-1; i >= 0; i--) {
+        out += arr[i];
+    }
+    return out;
 };
 
 // Do not edit below this line
