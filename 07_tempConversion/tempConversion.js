20fd09d2a3ea3bde644837ee7c44eed033c2b55d
Andrew Jia <andrew.jia.2013@gmail.com>
2022-06-27 16:51:54 -0700

    complete 07
    rounders!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let out = f;
+  out -= 32;
+  out *= (5/9);
+  out = Math.round(out * 10) / 10; //round to 1 place
+  return out;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let out = c;
+  out /= (5/9);
+  out += 32;
+  out = Math.round(out * 10) / 10; //round to 1 place
+  return out;
 };
 
 // Do not edit below this line
