8b992a160b681f6513b43aba6062fb632aa39747
Andrew Jia <andrew.jia.2013@gmail.com>
2022-07-05 14:39:37 -0700

    complete 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	//remove spaces and punc, then lower case
+	trim = str.replace( /\s|\.|\!|\?|\,/g, '').toLowerCase();
+	console.log(trim);
 
+	//compare leftmost and rightmost chars, then move to middle by 1
+	let len = trim.length;
+	let left = 0;
+	let right = len - 1;
+	while(left < right) {
+		if(trim.charAt(left) != trim.charAt(right)) {
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
 };
 
 // Do not edit below this line
