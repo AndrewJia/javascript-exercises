ea51f9cb3a3b202b4a2191c79894b4c26375640e
Andrew Jia <andrew.jia.2013@gmail.com>
2022-07-05 14:15:18 -0700

    complete 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for(a of arr) {
+    sum += a;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let res = 1;
+  for(a of arr) {
+    res *= a;
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+	if(pow === 0) return 1;
+  let res = 1;
+  for(let i = 0; i < pow; i++) {
+    res *= base;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let res = 1;
+  for(let i = 1; i <= a; i++) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
