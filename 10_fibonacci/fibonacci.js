0db7fd85d0398b47ccbac6c03547c02db3c7702e
Andrew Jia <andrew.jia.2013@gmail.com>
2022-07-05 16:02:17 -0700

    complete 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+	//convert string to number
+	if(typeof input != 'number') {
+		input = parseInt(input);
+	}
+	//negative invalid input
+	if(input < 0) return "OOPS";
+	if(input < 3) return 1;
+	let small = 1;
+	let large = 1;
+	for(let i = 3; i <= input; i++) {
+		//console.log("small, large at start: "+small+", "+large);
+		small = small + large;
+		let temp = small;
+		small = large;
+		large = temp;
+	}
+	return large;
 };
 
 // Do not edit below this line
