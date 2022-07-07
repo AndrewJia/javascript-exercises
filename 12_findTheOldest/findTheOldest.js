f9fa40a14b9ca297e368979bf9f718e820848a8a
Andrew Jia <andrew.jia.2013@gmail.com>
2022-07-06 22:12:30 -0700

    complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxAge = 0;
+    let oldestPerson = undefined;
+    for(person of people) {
+        let age;
+        if ("yearOfDeath" in person) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = new Date().getFullYear() - person.yearOfBirth;
+        }
+        if(age > maxAge) {
+            maxAge = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
